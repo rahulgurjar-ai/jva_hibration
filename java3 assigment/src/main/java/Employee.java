5eb5f133cb265b4245f4012684cbc3f5d9cffc5d
rahulgurjar-ai <rahulgurjar8583@gmail.com>
2025-03-25 11:45:09 +0530

    Add files via upload

---
+++ java3 assigment/src/main/java/Employee.java
@@ -0,0 +1,31 @@
+
+
+
+public class Employee {
+	
+	int id;
+	String name;
+	int salery;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getsalery(){
+		return salery;
+	}
+	public void setsalery(){
+		this.salery = salery;
+	}
+	
+	
+
+}
